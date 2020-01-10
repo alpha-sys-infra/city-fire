ae358f68f48a6d114635fe9e36db28f6872024b3
yanchengpeng <870696873@qq.com>
2020-01-10 17:32:52 +0800

    1.0

---
+++ src/main/java/wangdao/ListNode.java
@@ -0,0 +1,11 @@
+package wangdao;
+
+/**
+ * @author ycp
+ * @date 2020/1/9
+ */
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+}
---
+++ src/main/java/wangdao/Tree_4/TreeNode.java
@@ -0,0 +1,9 @@
+package wangdao.Tree_4;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x;
+    }
+}
